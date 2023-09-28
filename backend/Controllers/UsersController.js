b1d5233067b4980b2711c8ccf68d1b18492832a9
GabrielGodoi987 <gabrielgodoi987@gmail.com>
2023-09-28 18:18:53 -0300

    'usersController'

---
+++ backend/Controllers/UsersController.js
@@ -0,0 +1,16 @@
+const db = require('../db/models/index');
+
+module.exports = {
+    createUser(req, res) {
+
+    },
+    updateUser(req, res) {
+
+    },
+    authenticateUser(req, res) {
+
+    },
+    deleteUser(req, res) {
+
+    }
+}
