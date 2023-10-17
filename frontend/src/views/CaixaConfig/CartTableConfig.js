8e70621137752cb4e8674918a37b292268597e9e
GabrielGodoi987 <gabrielgodoi987@gmail.com>
2023-10-17 16:18:20 -0300

    frontend preparado

---
+++ frontend/src/views/CaixaConfig/CartTableConfig.js
@@ -0,0 +1,34 @@
+export const columns = [
+    {
+        name: 'id',
+        field: row => row.id,
+        label: 'Id',
+        align: "center",
+        width: "100px",
+        require: true,
+    },
+    {
+        name: 'nome',
+        field: row => row.name,
+        label: 'Nome',
+        align: 'center',
+        width: '150px',
+        require: true
+    },
+    {
+        name: 'quantidade',
+        field: row => row.quantidade,
+        label: 'Quantidade',
+        align:'center',
+        width: '100px',
+        require: true
+    },
+    {
+        name: 'subtotal',
+        field: row => row.subtotal,
+        label: 'SubTotal',
+        align: 'center',
+        width: '100px',
+        require: true
+    }
+]
--- frontend/src/views/FrenteCaixa.vue
+++ frontend/src/views/FrenteCaixa.vue
@@ -1,13 +1,118 @@
 <template>
   <q-layout view="lHh lpR lFf">
+    <q-header class="shadow-3 text-center">
+      <q-toolbar inset class="q-gutter-sm">
+        <q-breadcrumbs class="bg-primary" v-for="(i, index) in menu" :key="index">
+          <q-breadcrumbs-el :icon="i.icon" :to="i.route" :label="i.name" />
+        </q-breadcrumbs>
+      </q-toolbar>
+    </q-header>
 
+
+    <q-page-container class="q-mt-xl">
+      <div class="row justify-around items-center" style="height: 100%;">
+        <!-- ================================================================== -->
+        <!-- Parte caixa eletrônico -->
+        <div class="col-md-4">
+          <div class="row justify-around">
+            <!-- primeiro input, para buscar e selecionar os Clientes, para que seja contabilizado a compra do mesmo -->
+            <div class="col-md-10">
+              <!-- botão q-select com dialog-->
+              <q-select dense filled use-input label="Cliente" :options="options" @filter="filterFn" behavior="dialog"
+                class="q-mt-md">
+                <template v-slot:no-option>
+                  <q-item>
+                    <q-item-section class="text-grey">
+                      No results
+                    </q-item-section>
+                  </q-item>
+                </template>
+              </q-select>
+            </div>
+            <div class="col-md-1">
+              <q-btn square icon="add" class="q-mt-md" />
+            </div>
+          </div>
+          <!-- buscando produtos no banco de dados, caso não conseguir ver no painel ao lado vamos poder ver aqui -->
+          <!-- primeiro input, para buscar e selecionar os Clientes, para que seja contabilizado a compra do mesmo -->
+          <!-- botão q-select com dialog-->
+          <q-select dense filled :options="options" class="q-mt-md" />
+          <!-- ------------------------------ -->
+          <q-table class="q-mt-xl" :columns="CartConfig.columns">
+
+          </q-table>
+
+          <div class="row q-mt-xl justify-around">
+            <div class="col-md-4">
+              <q-input dense standout="bg-primary" label="teste" />
+            </div>
+            <div class="col-md-4">
+              <q-input dense standout="bg-primary" label="teste" />
+            </div>
+          </div>
+
+          <div class="row justify-around q-mt-xl">
+            <div class="col-md-5">
+              <q-btn square label="Pagar" color="primary" style="width: 100%;"/>
+            </div>
+            <div class="col-md-5">
+              <q-btn square label="cancelar" color="secondary" style="width: 100%;"/>
+            </div>
+          </div>
+        </div>
+
+        <!-- Fim Primeira div ============================================================== -->
+
+        <q-separator vertical color="dark" />
+
+        <!-- começo da div 2 ============================================================ -->
+        <div class="col-md-6 text-center">
+          <div class="row justify-around q-mb-xl">
+            <div class="col-md-5">
+              <q-select dense filled use-input label="Produto" :options="options" @filter="filterFn" behavior="dialog" />
+            </div>
+            <div class="col-md-5">
+              <q-select dense filled use-input label="Categoria" :options="options" @filter="filterFn"
+                behavior="dialog" />
+            </div>
+          </div>
+          <q-table grid class="q-mt-xl">
+
+          </q-table>
+        </div>
+        <!-- fim da segunda div ======================================================================== -->
+      </div>
+    </q-page-container>
   </q-layout>
 </template>
 
 <script>
-export default{
-    setup(){
-        
+import * as CartConfig from './CaixaConfig/CartTableConfig';
+export default {
+  setup() {
+
+    const menu = [
+      {
+        name: 'Dashboard',
+        icon: 'fa-solid fa-chart-simple',
+        route: '/dashboard'
+      },
+      {
+        name: 'Novos Produtos',
+        icon: 'fa-solid fa-folder',
+        route: '/productsRegister'
+      },
+      {
+        name: 'Produtos',
+        icon: 'fa-solid fa-bag-shopping',
+        route: '/allProducts'
+      }
+    ]
+
+    return {
+      CartConfig,
+      menu
     }
+  }
 }
 </script>
